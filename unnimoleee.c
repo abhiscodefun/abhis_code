05a1932dec693a0a2c37cf5047f2fc21f5efc491
abhicodeandchill <159055585+abhicodeandchill@users.noreply.github.com>
2024-02-05 23:19:07 +0530

    Update unnimoleee.c

--- unnimoleee.c
+++ unnimoleee.c
@@ -10,7 +10,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 int main()
 {
-    
+    # type "U" to get the cute result!
     printf("Hey Honey, what's the first letter of your name? \n");
     char name;
     scanf("%c", &name);
