9544bf9100fb74720bd85a1f9871e82aba028365
abhicodeandchill <159055585+abhicodeandchill@users.noreply.github.com>
2024-02-05 23:16:41 +0530

    Add files via upload

---
+++ unnimoleee.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("Hey Honey, what's the first letter of your name? \n");
+    char name;
+    scanf("%c", &name);
+    
+    printf("Wow %c are Unique!! \n", name);
+
+
+    return 0;
+}
+
